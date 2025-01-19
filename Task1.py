02dd47d4c363d6cd7906ef3a5f70781574e28097
OliXDS <oliversygulski@gmail.com>
2025-01-19 17:07:12 +0000

    randomLib

--- Task1.py
+++ Task1.py
@@ -1,6 +1,22 @@
+# libraries
+import random
+
+
+
 # variables
 
 score = 0
 answer = 0
+count = 0
 totalQuestions = 10
+num1 = 0
+num2 = 0
 
+
+# functions
+
+def questions():
+
+    if count < totalQuestions:
+        num1 = random.randint(1,20)
+        num2 = random.randint(1,20)
